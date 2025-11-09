c7c50efc258e47773f368bfa02afa1f15e013125
Shaktindra Sahoo <shaktindra1111@gmail.com>
2025-11-09 19:49:27 +0530

    sad

--- kthEnc.cpp
+++ kthEnc.cpp
@@ -63,7 +63,6 @@ int main()
     vector<int> nodes = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
     Node* root = buildTree(nodes);
     preOrder(root);
-
     cout<<"\nLCA : "<<kthEnc(root,4,2);
    
 }
